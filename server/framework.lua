13c728da6609baca850f606f2254587e227d4c78
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-10-14 15:40:04 -0700

    Update framework.lua

--- server/framework.lua
+++ server/framework.lua
@@ -1,1 +1,16 @@
+if GetResourceState('es_extended') ~= 'missing' then
+    esx = false 
+else 
+    esx = true
+end
+if esx then 
+    -- esx 1.1 users
+-- ESX = nil
 
+-- TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+ESX = exports["es_extended"]:getSharedObject()
+print("[Zaps] Tuner Started. | Framework Loaded ESX.")
+else 
+print("[Zaps] Tuner Started. | Framework Loaded QB-CORE.")
+  QBCore = exports['qb-core']:GetCoreObject()
+end
