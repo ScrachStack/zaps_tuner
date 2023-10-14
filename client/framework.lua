a32458f7bf82b1c89ebeb3d5972143cf8bbea9cb
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-10-14 15:02:00 -0700

    Create framework.lua

---
+++ client/framework.lua
@@ -0,0 +1,22 @@
+if GetResourceState('es_extended') ~= 'missing' then
+    esx = false 
+else 
+    esx = true
+end
+if esx then 
+ESX = exports["es_extended"]:getSharedObject()
+print("[Zaps] Tuner Started. | Framework Loaded ESX.")
+-- for users on esx 1.1
+--CreateThread(function()
+  --  while ESX == nil do
+    --    TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+      --  Citizen.Wait(0)
+    --end
+--end)
+else 
+
+print("[Zaps] Tuner Started. | Framework Loaded QB-CORE.")
+
+  QBCore = exports['qb-core']:GetCoreObject()
+
+end
