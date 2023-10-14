fa6b2f4055cd0ff08102b83385abc9e814ef0f67
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-10-14 15:33:34 -0700

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,13 @@
+Config = {}
+
+Config.Locale = 'en'
+
+Config.Item = 'tunerchip' -- Tuner Chip Item Name
+
+
+Locales = {
+    ['en'] = {
+        ['chip_used'] = 'Tuner Chip applied to %s',
+        ['no_vehicle'] = 'You are not in a vehicle!',
+    }
+}
