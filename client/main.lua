9091a22c7b344082e68c1189d2d72ce3aa3c525b
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-10-14 19:29:33 -0700

    Added plate change command

--- client/main.lua
+++ client/main.lua
@@ -36,3 +36,54 @@ AddEventHandler('zaps:useChip', function()
         })
     end
 end)
+
+if Config.UsePlateChangeCommand then 
+RegisterCommand('changeplate', function()
+    local input = lib.inputDialog('Change Plate', { {type = 'input', label = 'Enter your plate', required = true, min = 1, max = 8} })
+
+    if not input or not input[1] then
+        lib.notify({
+            title = 'Change Plate',
+            description = 'No plate entered.',
+            position = 'top',
+            style = {
+                backgroundColor = '#141517',
+                color = '#C1C2C5',
+            },
+            icon = 'times',
+            iconColor = '#C53030'
+        })
+        return
+    end
+
+    local playerPed = PlayerPedId()
+    local vehicle = GetVehiclePedIsIn(playerPed, false)
+
+    if vehicle and vehicle ~= 0 then
+        SetVehicleNumberPlateText(vehicle, input[1])
+        lib.notify({
+            title = 'Change Plate',
+            description = 'Plate changed successfully.',
+            position = 'top',
+            style = {
+                backgroundColor = '#141517',
+                color = '#C1C2C5',
+            },
+            icon = 'check',
+            iconColor = '#29a329'
+        })
+    else
+        lib.notify({
+            title = 'Change Plate',
+            description = 'You are not in a vehicle.',
+            position = 'top',
+            style = {
+                backgroundColor = '#141517',
+                color = '#C1C2C5',
+            },
+            icon = 'times',
+            iconColor = '#C53030'
+        })
+    end
+end, false)
+end
